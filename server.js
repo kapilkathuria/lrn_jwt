2438fe39c9dbafa67e4b012e13bcda5baac3b6f2
kapil <kapil.kathuria@gmail.com>
2020-08-21 16:32:39 +0530

    added app and error middle ware iin server,js

--- server.js
+++ server.js
@@ -5,13 +5,33 @@ dotenv.config();
 
 const app = express();
 
+const myMiddleware = (res, req, next) => {
+    console.log("hey i am middleware");
+    next();
+};
+
+// application level middleware
+app.use(myMiddleware);
+// use above or below
+// app.get('/', myMiddleware, (req,res,ne) 
+
+app.get('/', (req,res,ne) => {
+    res.send("hello from node api server");
+});
+
 // use port from dotenv, if not specified than use 3000
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res,ne) => {
-    res.send("hello from node server");
-})
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+});
+
+// error level middleware
+app.use(function(err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {}
+    });
 });
