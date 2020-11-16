91358449999487bc0150b88c39652e7f616eb162
kapil <kapil.kathuria@gmail.com>
2020-11-16 19:12:41 +0530

    minor changes during course review

---
+++ section4.js
@@ -0,0 +1,7 @@
+// Course: https://www.udemy.com/course/nodejs-rest-api-jwt-security-mongodb-complete-guide
+// 2 key services will be created with following endpoints:
+//  /user:/user/login; /user/signup
+//  /product: /product(post, get), /product/:id (get, put, delete)
+// 
+// swagger url: http://localhost:3002/api-docs/#/
+
